77b7343427c75c6dd2e9ca49e3f500d7438d1719
PikalaxALT <pikalaxalt@gmail.com>
2018-02-12 15:07:00 -0500

    Add clean instructions to envsetup

--- envsetup.sh
+++ envsetup.sh
@@ -1,4 +1,8 @@
+#!/bin/sh
+
 set -x
+rm -f aclocal.m4 compile config.guess config.log config.status config.sub configure \
+	install-sh Makefile.in missing INSTALL COPYING
 aclocal
 autoconf
 automake --add-missing
